9521e897aa4310886eddcd4a50ecebd5d44be2bc
kittipich63 <107613667+kittipich63@users.noreply.github.com>
2023-05-22 14:31:29 +0700

    edit addbooking

--- myapp_user/forms.py
+++ myapp_user/forms.py
@@ -13,8 +13,11 @@ class BookingModelForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
         self.fields['room'].queryset = MyRoom.objects.filter(status='เปิด')
         if self.instance:
-            self.initial['start_time'] = self.instance.start_time.strftime('%H:%M')
-            self.initial['end_time'] = self.instance.end_time.strftime('%H:%M')
+            if self.instance.start_time:
+                self.initial['start_time'] = self.instance.start_time.strftime('%H:%M')
+            if self.instance.end_time:
+                self.initial['end_time'] = self.instance.end_time.strftime('%H:%M')
+
 
     class Meta:
         model = Booking
